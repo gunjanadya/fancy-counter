64e37d946a131c85885405ebb067f78a458db26d
Gunjan Adya <gunjan.adya@gmail.com>
2024-02-08 21:00:15 -0500

    Add files via upload

---
+++ clock_div.vhd
@@ -0,0 +1,68 @@
+----------------------------------------------------------------------------------
+-- Company: Rutgers
+-- Engineer: Gunjan Adya
+-- 
+-- Create Date: 02/08/2024 07:42:09 PM
+-- Design Name: 
+-- Module Name: clock_div - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity clock_div is
+port (
+  clk : in std_logic;
+  cnt : out std_logic
+);
+end clock_div;
+
+architecture Behavioral of clock_div is
+
+  signal count : std_logic_vector (26 downto 0) := (others => '0');
+
+begin
+  
+  process(clk) begin
+    count <= "000000000000000000000000000";
+    if rising_edge(clk) then
+
+        if (unsigned(count) < 62499999) then
+            cnt <= '0';
+            count <= std_logic_vector(unsigned(count) + 1);
+            
+        --elsif (unsigned(count) = 62499999) then
+            
+        else 
+            cnt  <= '1';
+            count <= (others => '0');
+            
+        end if;
+
+
+    end if;
+  end process;
+
+end Behavioral;
---
+++ clock_div_tb.vhd
@@ -0,0 +1,72 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Gunjan Adya
+-- 
+-- Create Date: 02/08/2024 08:24:05 PM
+-- Design Name: 
+-- Module Name: clock_div_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity clock_div_tb is
+--  Port ( );
+end clock_div_tb;
+
+architecture Behavioral of clock_div_tb is
+
+    signal tb_clk : std_logic := '0';
+    signal tb_cnt : std_logic := '0';
+
+
+
+    component clock_div is
+        port(
+        
+            clk  : in std_logic;        -- 125 Mhz clock
+            cnt  : out std_logic
+        
+        );
+    end component ;
+
+begin
+
+    clk_gen_proc: process
+    begin
+    
+        wait for 4 ns;
+        tb_cnt <= '1';
+        
+        wait for 4 ns;
+        tb_cnt <= '0';
+    
+    end process clk_gen_proc;
+dut : clock_div
+port map (
+    clk  => tb_clk,
+    cnt => tb_cnt
+);
+
+end Behavioral;
